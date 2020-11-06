a5c658c60e06108c2471cad81a2c0048019c3a45
Delano tracey <delanotracey@gmail.com>
2020-11-06 15:15:47 -0500

    exercise 2 11/6/2020

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
 window.onload= function(){
     var btn=document.getElementsByTagName("button")[0];
+    var result=document.getElementById("result");
+    var searchtxt=document.getElementsByName("searchtxt")[0];
 
+    
     btn.onclick= function(){
         var httpRequest= new XMLHttpRequest();
-        var url="http:///localhost:8080/superheroes.php";
-        httpRequest.onreadystatechange=dothis;
-        httpRequest.open('GET',url);
-        httpRequest.send();
+        var url;
+        if(searchtxt.value!=null){
+            url="http:///localhost:8080/superheroes.php"+"?query="+searchtxt.value;
+            httpRequest.onreadystatechange=dothis;
+            httpRequest.open('GET',url);
+            httpRequest.send();
+        }else{
+            url="http:///localhost:8080/superheroes.php";
+            httpRequest.onreadystatechange=dothis;
+            httpRequest.open('GET',url);
+            httpRequest.send();
+             
+        }
         function dothis(){
             if (httpRequest.readyState === XMLHttpRequest.DONE) {
                 if (httpRequest.status === 200) {
-                var response = httpRequest.responseText;
-                alert(response);
+                    var response = httpRequest.responseText;
+                    result.innerHTML=response;
                 } else {
                 alert('There was a problem with the request.');
                 }
             }
-        }
-        
-        
+        }  
     }
 }
--- superheroes.php
+++ superheroes.php
@@ -66,7 +66,35 @@ $superheroes = [
 ?>
 
 <ul>
-<?php foreach ($superheroes as $superhero): ?>
-  <li><?= $superhero['alias']; ?></li>
-<?php endforeach; ?>
-</ul>
+<?php // foreach ($superheroes as $superhero):?> 
+  <li><? //= $superhero['alias']; ?></li>
+<?php // endforeach; ?>
+</ul> 
+
+
+
+
+
+
+<?php $q=$_GET['query'];?>
+<?php $s=0 ;?>
+<?php if (empty($q)){?>
+        <ul>
+        <?php foreach($superheroes as $superhero){?>
+            <li><?=$superhero['alias'];?></li>
+        <?php } ?>
+        </ul>
+    <?php } else {  ?>
+        <?php foreach($superheroes as $superhero){ ?>
+            <?php if( $q==$superhero['alias'] || $q==$superhero['name']){?>
+                <? $s=1; ?>
+                <h3><?=$superhero['alias'];?></h3>
+                <h4><?=$superhero['name'];?></h4>
+                <p><?= $superhero['biography'];?></p>
+           <?php } ?> 
+        <?php } ?>
+        <?php if($s==0){?>
+            <h3 style="color:#C72523"><?php echo "SUPERHERO NOT FOUND";?></h3>
+        <?php }?>
+    <?php } ?>  
+    
